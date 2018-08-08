584f86279c6a57ec910be19452b38427fecbce0c
Y-sir <577790911@qq.com>
2018-08-08 16:27:00 +0800

    test_github_streak

--- green.py
+++ green.py
@@ -32,4 +32,4 @@ def daily_commit(start_date, end_date):
 
 
 if __name__ == '__main__':
-    daily_commit(datetime.date(2018, 3, 8), datetime.date(2018, 12, 24))
+    daily_commit(datetime.date(2018, 8, 8), datetime.date(2018, 12, 24))
